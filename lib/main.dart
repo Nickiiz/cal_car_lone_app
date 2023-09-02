6faa80f5cc2ba0f8a19a6b0e35e959a959322eb4
Nickiiz <s6319c10004@sau.ac.th>
2023-09-02 13:35:59 +0700

    first commmit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:cal_car_loan_app/views/introduction_ui.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IntroductionUI(),
+    ),
+  );
+}
