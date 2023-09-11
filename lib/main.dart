6f7075a14690f60366ab45baa56ed39d71ce16d9
Nickiiz <s6319c10004@sau.ac.th>
2023-09-11 14:21:52 +0700

    update introductionui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:cal_car_loan_app/views/home_ui.dart';
 import 'package:cal_car_loan_app/views/introduction_ui.dart';
 import 'package:flutter/material.dart';
 
